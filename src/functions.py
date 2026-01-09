07509d650a6bd7ca897344fcde4423a69f42c0fc
Utsav Deep <utsavdeep01@gmail.com>
2026-01-09 12:24:44 +0100

    add function to extract textnodes from exisiting textnodes

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,4 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 from htmlnode import LeafNode
 
 
@@ -6,7 +6,7 @@ def text_node_to_html_node(text_node) -> LeafNode:
     """
     Converts a TextNode to an HTMLNode specifically a LeafNode
 
-    Accepts:
+    accepts:
         - text_node: A TextNode object
 
     return:
@@ -34,3 +34,41 @@ def text_node_to_html_node(text_node) -> LeafNode:
         case _:
             # raise exception on any other TextType
             raise Exception(f"{text_node} is not a valid TextNode!")
+
+
+def split_nodes_delimiter(old_nodes: list[TextNode], dilimiter: str, text_type: TextType) -> list:
+    """
+    Accepts a list of TextNodes extracts any inline nodes that might be presesnt using the passed dilimiter.
+
+    accepts:
+        - old nodes: a list of TextNodes
+        - dilimiter: a string representing a dilimiter
+        - text_type: TextType for the extraction
+
+    return:
+        - a list of TextNodes extracted from the old nodes
+    """
+    extracted_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            extracted_nodes.append(node)
+            continue
+
+        if node.text.count(dilimiter) % 2 != 0:
+            raise Exception(f"Invalid Markdown Syntax! Matching {
+                            dilimiter} was not found in {node.text}.")
+
+        new_nodes = []
+        split_strings = node.text.split(dilimiter)
+        for i in range(len(split_strings)):
+            if split_strings[i] == "":
+                continue
+            if i % 2 != 0:
+                new_nodes.append(TextNode(split_strings[i], text_type))
+            else:
+                new_nodes.append(
+                    TextNode(split_strings[i], TextType.PLAIN_TEXT))
+        extracted_nodes.extend(new_nodes)
+
+    return extracted_nodes
