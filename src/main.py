501e1d9b0afb212b9974dda78c6b765b34634e47
Utsav Deep <utsavdeep01@gmail.com>
2026-01-11 08:27:35 +0100

    add some more pages to the site

--- src/main.py
+++ src/main.py
@@ -24,6 +24,8 @@ def generate_page(from_path: os.PathLike, template_path: os.PathLike, dest_path:
 
     content = template_content.replace("{{ Title }}", title).replace(
         "{{ Content }}", html_content)
+    print(os.makedirs(os.path.dirname(os.path.join(
+        os.path.abspath("."), dest_path)), exist_ok=True))
 
     with open(dest_path, "w") as dest_file:
         dest_file.write(content)
@@ -33,6 +35,14 @@ def main():
     copy_dir_to_public()
     generate_page(from_path="content/index.md",
                   template_path="template.html", dest_path="public/index.html")
+    generate_page(from_path="content/blog/glorfindel/index.md",
+                  template_path="template.html", dest_path="public/blog/glorfindel/index.html")
+    generate_page(from_path="content/blog/majesty/index.md",
+                  template_path="template.html", dest_path="public/blog/majesty/index.html")
+    generate_page(from_path="content/blog/tom/index.md",
+                  template_path="template.html", dest_path="public/blog/tom/index.html")
+    generate_page(from_path="content/contact/index.md",
+                  template_path="template.html", dest_path="public/contact/index.html")
 
 
 if __name__ == "__main__":
