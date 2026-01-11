635d5dded8c8963c8032d80b9060bd6ae7cc81e8
Utsav Deep <utsavdeep01@gmail.com>
2026-01-11 08:52:20 +0100

    generate pages recursively

--- src/main.py
+++ src/main.py
@@ -24,25 +24,34 @@ def generate_page(from_path: os.PathLike, template_path: os.PathLike, dest_path:
 
     content = template_content.replace("{{ Title }}", title).replace(
         "{{ Content }}", html_content)
-    print(os.makedirs(os.path.dirname(os.path.join(
-        os.path.abspath("."), dest_path)), exist_ok=True))
+    os.makedirs(os.path.dirname(os.path.join(
+        os.path.abspath("."), dest_path)), exist_ok=True)
 
     with open(dest_path, "w") as dest_file:
         dest_file.write(content)
 
 
+def generate_page_recursive(dir_path_content="content/", template_path="template.html", dest_dir_path="public/"):
+    # get abs paths
+    src_path = os.path.join(os.path.abspath("."), dir_path_content)
+    template = os.path.join(os.path.abspath("."), template_path)
+    dest_dir = os.path.join(os.path.abspath("."), dest_dir_path)
+    # copy contents
+    items = os.listdir(src_path)
+    for item in items:
+        current_item_path = os.path.join(src_path, item)
+        if os.path.isfile(current_item_path):
+            generate_page(current_item_path, template,
+                          os.path.join(dest_dir_path, item.replace(".md", ".html")))
+        else:
+            new_dest_path = os.path.join(dest_dir, item)
+            generate_page_recursive(
+                current_item_path, template_path, new_dest_path)
+
+
 def main():
     copy_dir_to_public()
-    generate_page(from_path="content/index.md",
-                  template_path="template.html", dest_path="public/index.html")
-    generate_page(from_path="content/blog/glorfindel/index.md",
-                  template_path="template.html", dest_path="public/blog/glorfindel/index.html")
-    generate_page(from_path="content/blog/majesty/index.md",
-                  template_path="template.html", dest_path="public/blog/majesty/index.html")
-    generate_page(from_path="content/blog/tom/index.md",
-                  template_path="template.html", dest_path="public/blog/tom/index.html")
-    generate_page(from_path="content/contact/index.md",
-                  template_path="template.html", dest_path="public/contact/index.html")
+    generate_page_recursive()
 
 
 if __name__ == "__main__":
